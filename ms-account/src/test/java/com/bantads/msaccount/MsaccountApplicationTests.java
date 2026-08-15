e930bab13a8f31256b00def3c05e99dc678aa35a
Lara Reis <laraglufke@gmail.com>
2026-08-15 19:18:36 -0300

    Initial commit

---
+++ ms-account/src/test/java/com/bantads/msaccount/MsaccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bantads.msaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
