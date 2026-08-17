3cf104087ced4a164ea34e222a22eb5337922bb9
gabi-gandine <gabriela.gandine@gmail.com>
2026-08-17 12:59:37 -0300

    fix: fix ms account imports

--- ms-account/src/main/java/com/bantads/entity/event/Event.java
+++ ms-account/src/main/java/com/bantads/entity/event/Event.java
@@ -1,11 +1,15 @@
 package com.bantads.entity.event;
 
+import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
+
 public class Event {
     private UUID eventId;
     private String objectId;
     private EventType eventType;
     private long version;
     private Map payload ;
-    private LocalDateTime createdAt;
+    private Date createdAt;
 
 }
--- ms-account/src/main/java/com/bantads/entity/read/AccountHistory.java
+++ ms-account/src/main/java/com/bantads/entity/read/AccountHistory.java
@@ -1,6 +1,7 @@
 package com.bantads.entity.read;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 public class AccountHistory {
     private Long historyId;
