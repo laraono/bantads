8b897409613f6500148f23c8f5430c891d82e7c6
gabi-gandine <gabriela.gandine@gmail.com>
2026-08-23 11:59:07 -0300

    feat: add client dto

---
+++ ms-client/src/main/java/com/bantads/dto/ClientDTO.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bantads.dto;
+
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author lenovo
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ClientDTO {
+    private String name;
+
+    private String email;
+
+    private String cpf;
+
+    private String phone;
+
+    private BigDecimal salary;
+    
+    private AddressDTO address;
+}
