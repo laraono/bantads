9ad8af84e6a1b73c2f3eca402f0c8e7d158ae7bb
gabi-gandine <gabriela.gandine@gmail.com>
2026-08-23 16:30:36 -0300

    feat: add schemas configs

---
+++ ms-account/src/main/java/com/bantads/config/PersistenceCommandConfiguration.java
@@ -0,0 +1,77 @@
+package com.bantads.config;
+
+import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+
+@Configuration
+@PropertySource({ "classpath:persistence-multiple-db.properties" })
+@EnableJpaRepositories(
+        basePackages = "com.bantads.repository.event",
+        entityManagerFactoryRef = "commandEntityManager",
+        transactionManagerRef = "commandTransactionManager"
+)
+public class PersistenceCommandConfiguration {
+    @Autowired
+    private Environment env;
+
+    @Bean
+    @Primary
+    public LocalContainerEntityManagerFactoryBean commandEntityManager() {
+        LocalContainerEntityManagerFactoryBean em
+                = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(commandDataSource());
+        em.setPackagesToScan(
+                new String[] { "com.bantads.entity.event" });
+
+        HibernateJpaVendorAdapter vendorAdapter
+                = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        HashMap<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.hbm2ddl.auto",
+                env.getProperty("hibernate.hbm2ddl.auto"));
+        properties.put("hibernate.dialect",
+                env.getProperty("hibernate.dialect"));
+        em.setJpaPropertyMap(properties);
+
+        return em;
+    }
+
+    @Primary
+    @Bean
+    public DataSource commandDataSource() {
+
+        DriverManagerDataSource dataSource
+                = new DriverManagerDataSource();
+        dataSource.setDriverClassName(
+                env.getProperty("jdbc.driverClassName"));
+        dataSource.setUrl(env.getProperty("user.jdbc.url"));
+        dataSource.setUsername(env.getProperty("jdbc.user"));
+        dataSource.setPassword(env.getProperty("jdbc.pass"));
+
+        return dataSource;
+    }
+
+    @Primary
+    @Bean
+    public PlatformTransactionManager commandTransactionManager() {
+
+        JpaTransactionManager transactionManager
+                = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(
+                commandEntityManager().getObject());
+        return transactionManager;
+    }
+}
