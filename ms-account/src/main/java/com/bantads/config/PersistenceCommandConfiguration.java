e254bf7afe7d966e1cbcc5fedb0d0cf78d4bad4a
gabi-gandine <gabriela.gandine@gmail.com>
2026-08-23 16:33:27 -0300

    refactor: change enviroment import

--- ms-account/src/main/java/com/bantads/config/PersistenceCommandConfiguration.java
+++ ms-account/src/main/java/com/bantads/config/PersistenceCommandConfiguration.java
@@ -1,6 +1,6 @@
 package com.bantads.config;
 
-import org.hibernate.cfg.Environment;
+import org.springframework.core.env.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
