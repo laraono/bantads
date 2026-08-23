3873947875a99ac104ee14bce036b3e54bf3457c
gabi-gandine <gabriela.gandine@gmail.com>
2026-08-23 11:59:59 -0300

    feat: add get request by client

--- ms-client/src/main/java/com/bantads/repository/RequestRepository.java
+++ ms-client/src/main/java/com/bantads/repository/RequestRepository.java
@@ -1,9 +1,10 @@
 package com.bantads.repository;
 
+import com.bantads.entity.Client;
 import com.bantads.entity.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RequestRepository extends JpaRepository<Request, Long> {
 
-
+    Request findByClient(Client client);
 }
