84f9cc09f468fa8dba3a6df3b3293f35d4af007d
Leagueofcorno <cauadias.henrique@gmail.com>
2026-08-26 05:09:02 -0300

    feature - 9.2

--- ms-auth/src/main/java/com/bantads/msauth/service/AuthService.java
+++ ms-auth/src/main/java/com/bantads/msauth/service/AuthService.java
@@ -1,70 +1,87 @@
 package com.bantads.msauth.service;
+
 import com.bantads.msauth.entity.Auth;
 import com.bantads.msauth.repository.AuthRepository;
+
 import com.password4j.Password;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
 import java.util.List;
 
 @Service
-public class AuthService 
-{
- private final AuthRepository authRepository;
- public AuthService(AuthRepository authRepository) 
-    {
-      this.authRepository = authRepository;
+public class AuthService {
+
+    private final AuthRepository authRepository;
+
+    public AuthService(AuthRepository authRepository) {
+        this.authRepository = authRepository;
     }
-    public Auth authenticate(String login, String password) 
-    {
-    Auth auth = authRepository.findByLogin(login).orElseThrow(() -> new ResponseStatusException
-        (
-         HttpStatus.UNAUTHORIZED,"NÃO AUTORIZADO"
-        ));
-        if (!Boolean.TRUE.equals(auth.getActive())) 
-        {
-         throw new ResponseStatusException
-            (
-             HttpStatus.UNAUTHORIZED,"NÃO AUTORIZADO"
+
+    public Auth authenticate(String login, String password) {
+
+        Auth auth = authRepository.findByLogin(login)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.UNAUTHORIZED,
+                        "NÃO AUTORIZADO"
+                ));
+
+        if (!Boolean.TRUE.equals(auth.getActive())) {
+            throw new ResponseStatusException(
+                    HttpStatus.UNAUTHORIZED,
+                    "NÃO AUTORIZADO"
             );
         }
-        boolean passwordValid = Password.check(password,auth.getPassword()).withArgon2();
-        if (!passwordValid) 
-        {
-            throw new ResponseStatusException
-            (
-                HttpStatus.UNAUTHORIZED,"NÃO AUTORIZADO"
+
+        boolean passwordValid = Password
+                .check(password, auth.getPassword())
+                .withArgon2();
+
+        if (!passwordValid) {
+            throw new ResponseStatusException(
+                    HttpStatus.UNAUTHORIZED,
+                    "NÃO AUTORIZADO"
             );
         }
-    return auth;
+
+        return auth;
     }
-    public Auth create
-    (
-     String cpf,
-     String type,
-     String login,
-     String rawPassword,
-     Boolean active
-    ) 
-    {
-        if (authRepository.existsByLogin(login)) 
-        {
-         throw new ResponseStatusException
-         (
-          HttpStatus.CONFLICT,"CONTA JÁ EXISTE"
-         );
+
+    public Auth create(
+            String cpf,
+            String type,
+            String login,
+            String rawPassword,
+            Boolean active
+    ) {
+
+        if (authRepository.existsByLogin(login)) {
+            throw new ResponseStatusException(
+                    HttpStatus.CONFLICT,
+                    "CONTA JÁ EXISTE"
+            );
         }
-    Auth auth = new Auth();
-    auth.setCpf(cpf);
-    auth.setType(type);
-    auth.setLogin(login);
-    auth.setActive(active);
-    String hash = Password.hash(rawPassword).withArgon2().getResult();
-    auth.setPassword(hash);
-    return authRepository.save(auth);
+
+        Auth auth = new Auth();
+
+        auth.setCpf(cpf);
+        auth.setType(type);
+        auth.setLogin(login);
+        auth.setActive(active);
+
+        String hash = Password
+                .hash(rawPassword)
+                .withArgon2()
+                .getResult();
+
+        auth.setPassword(hash);
+
+        return authRepository.save(auth);
     }
-    public List<Auth> findAll() 
-    {
-     return authRepository.findAll();
+
+    public List<Auth> findAll() {
+        return authRepository.findAll();
     }
 }
