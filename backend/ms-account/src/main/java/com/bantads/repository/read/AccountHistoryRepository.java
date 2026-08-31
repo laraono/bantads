5f67eff13bb345056bc649a2416f976f4f46af8c
gabi-gandine <gabriela.gandine@gmail.com>
2026-08-31 09:49:02 -0300

    feat: add request repository

--- backend/ms-account/src/main/java/com/bantads/repository/read/AccountHistoryRepository.java
+++ backend/ms-account/src/main/java/com/bantads/repository/read/AccountHistoryRepository.java
@@ -3,5 +3,8 @@ package com.bantads.repository.read;
 import com.bantads.entity.read.AccountHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AccountHistoryRepository extends JpaRepository<AccountHistory, Long> {
+    List<AccountHistory> findAllByAccountNumber(String number);
 }
