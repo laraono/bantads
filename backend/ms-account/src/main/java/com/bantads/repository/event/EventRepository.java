1db977e4fbd11aac956e2a7d6924a76460a97e28
gabi-gandine <gabriela.gandine@gmail.com>
2026-08-31 09:49:18 -0300

    feat: add custom event repository functions

--- backend/ms-account/src/main/java/com/bantads/repository/event/EventRepository.java
+++ backend/ms-account/src/main/java/com/bantads/repository/event/EventRepository.java
@@ -1,8 +1,16 @@
 package com.bantads.repository.event;
 
 import com.bantads.entity.event.Event;
+import com.bantads.entity.event.EventType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
-public interface EventRepository extends JpaRepository<Event, UUID> { }
+public interface EventRepository extends JpaRepository<Event, UUID> {
+    Event findFirstByObjectIdOrderByCreatedAtDesc(String objectId);
+
+    List<Event> findAllByObjectIdOrderByVersionAsc(String objectId);
+
+    boolean existsByObjectIdAndEventType(String objectId, EventType eventType);
+}
