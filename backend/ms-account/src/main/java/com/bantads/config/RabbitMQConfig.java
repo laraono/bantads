9d90a5609d79f6a5fd8d3393a97c84432209391a
gabi-gandine <gabriela.gandine@gmail.com>
2026-09-01 17:59:02 -0300

    refactor: remove comment

--- backend/ms-account/src/main/java/com/bantads/config/RabbitMQConfig.java
+++ backend/ms-account/src/main/java/com/bantads/config/RabbitMQConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
 
-    public static final String QUEUE_NAME = "ms.conta.events"; // Fixed trailing parenthesis
+    public static final String QUEUE_NAME = "ms.conta.events";
 
     @Bean
     public Queue readModelQueue() {
