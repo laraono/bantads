0e34a8ff732c219a4f7e317b50a77ac6e56b90df
gabi-gandine <gabriela.gandine@gmail.com>
2026-09-07 15:39:41 -0300

    refactor: remove schema from entity

--- backend/ms-account/src/main/java/com/bantads/entity/read/AccountHistory.java
+++ backend/ms-account/src/main/java/com/bantads/entity/read/AccountHistory.java
@@ -5,10 +5,11 @@ import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
-@Table(name = "account_history", schema = "request_db")
+@Table(name = "account_history")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -29,7 +30,7 @@ public class AccountHistory {
     @Column(nullable = false, name = "origin_client_cpf")
     private String originClientCPF;
 
-    @Column(nullable = false, name = "origin_client_name")
+    @Column(nullable = true, name = "origin_client_name")
     private String originClientName;
 
     @Column(nullable = true, name = "destination_client_cpf")
@@ -43,11 +44,11 @@ public class AccountHistory {
 
     @CreationTimestamp
     @Column(name = "created_at", updatable = false)
-    private Date createdAt;
+    private LocalDateTime createdAt;
 
-    @Column(nullable = false, name = "manager_cpf")
+    @Column(nullable = true, name = "manager_cpf")
     private String managerCPF;
 
-    @Column(nullable = false, name = "manager_name")
+    @Column(nullable = true, name = "manager_name")
     private String managerName;
 }
