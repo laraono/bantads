141190d0105fb90cdd231d13f9d2d991508ea5c9
gabi-gandine <gabriela.gandine@gmail.com>
2026-09-08 20:09:53 -0300

    feat: add controllers to all account operations

--- backend/api-gateway/src/controller/accountController.js
+++ backend/api-gateway/src/controller/accountController.js
@@ -3,13 +3,55 @@ const accountService = require('../service/accountService');
 class AccountController {
     async transfer(req, res) {
         try {
-            const result = await accountService.transfer(req.params.id, req.body);
+            const result = await accountService.transfer(req.params.id, req.body, req.headers);
             return res.status(201).json(result);
         } catch (err) {
             const status = err.status || 500;
             return res.status(status).json({ error: err.message || 'Falha no servidor' });
         }
     }
+
+    async withdraw(req, res) {
+        try {
+            const result = await accountService.withdraw(req.params.id, req.body, req.headers);
+            return res.status(201).json(result);
+        } catch (err) {
+            const status = err.status || 500;
+            return res.status(status).json({ error: err.message || 'Falha no servidor' });
+        }
+    }
+
+
+    async deposit(req, res) {
+        try {
+            const result = await accountService.deposit(req.params.id, req.body, req.headers);
+            return res.status(201).json(result);
+        } catch (err) {
+            const status = err.status || 500;
+            return res.status(status).json({ error: err.message || 'Falha no servidor' });
+        }
+    }
+
+    async getAccount(req, res) {
+        try {
+            const result = await accountService.getAccount(req.params.id, req.headers);
+            return res.status(200).json(result);
+        } catch (err) {
+            const status = err.status || 500;
+            return res.status(status).json({ error: err.message || 'Falha no servidor' });
+        }
+    }
+
+    async getExtract(req, res) {
+        try {
+            const result = await accountService.getExtract(req.params.id, req.body, req.headers);
+            return res.status(200).json(result);
+        } catch (err) {
+            const status = err.status || 500;
+            return res.status(status).json({ error: err.message || 'Falha no servidor' });
+        }
+    }
+
 
 }
 
--- backend/api-gateway/src/routes/accountRoutes.js
+++ backend/api-gateway/src/routes/accountRoutes.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware, fixRequestBody } = require('http-proxy-middleware');
 const {authGatewayFilter} = require('../middlewares/authGatewayMiddleware');
 const accountController = require('../controller/accountController');
 
@@ -9,30 +9,14 @@ const MS_ACCOUNT_HOST = process.env.MS_ACCOUNT_HOST || 'ms-account';
 const MS_ACCOUNT_PORT = process.env.MS_ACCOUNT_PORT || '8083';
 const target = `http://${MS_ACCOUNT_HOST}:${MS_ACCOUNT_PORT}`;
 
-router.get('/:id', authGatewayFilter(), createProxyMiddleware({
-    target,
-    changeOrigin: true,
-    pathRewrite: (path) => `/accounts${path}`
-}));
+router.get('/:id', authGatewayFilter(), accountController.getAccount);
 
-router.get('/:id/extrato', authGatewayFilter(), createProxyMiddleware({
-    target,
-    changeOrigin: true,
-    pathRewrite: (path, req) => { return`/accounts/${req.params.id}/extract/${req.query}` }
-}));
+router.post('/:id/deposito',  authGatewayFilter(), accountController.deposit);
 
-router.post('/:id/deposito', authGatewayFilter(), createProxyMiddleware({
-    target,
-    changeOrigin: true,
-    pathRewrite: (path, req) =>{ return`/accounts/${req.params.id}/deposit` }
-}));
+router.post('/:id/saque', authGatewayFilter(), accountController.withdraw);
 
-router.post('/:id/saque', authGatewayFilter(), createProxyMiddleware({
-    target,
-    changeOrigin: true,
-    pathRewrite: (path, req) =>{ return `/accounts/${req.params.id}/withdraw` }
-}));
+router.get('/:id/extrato',  authGatewayFilter(), accountController.getExtract);
 
-router.post('/:id/transferencia', accountController.transfer);
+router.post('/:id/transferencia',  authGatewayFilter(),  accountController.transfer);
 
 module.exports = router;
