673f3bf54d46f58ad228bbbf2664d94f174831dc
Lara Reis <laraglufke@gmail.com>
2026-09-14 01:52:39 -0300

    email implemented

---
+++ backend/ms-email/src/main/java/com/bantads/msemail/dto/EmailRequestDTO.java
@@ -0,0 +1,18 @@
+package com.bantads.msemail.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Builder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EmailRequestDTO {
+    private String to;
+    private String subject;
+    private String message;
+}
